e552af9e6ff59d349affa6cc914f0589fa2ca19f
xnorahn2001 <145736519+xnorahn2001@users.noreply.github.com>
2024-09-18 13:26:25 +0300

    resolve conflict

--- Program.cs
+++ Program.cs
@@ -7,34 +7,6 @@ app.UseHttpsRedirection();
 // create list of employees.
 List<Employee> employees = new List<Employee>();
 
-// Retrieve a specific employee by their unique Id//
-app.MapGet("/employees/{id}", (Guid id) =>
-{
-    var employee = employees.FirstOrDefault(employee => employee.Id 
-    == id);
-    if (employee is null){
-      return Results.NotFound("Employee is NotFound ");
-      
-    }
-  
-  return Results.Ok(employee);
-});
-
-//Delete an employee by their unique Id//
-    
-    app.MapDelete("/employees/{id}", (Guid id ) => 
-    {
-
-    var employee = employees.FirstOrDefault(employee => employee.Id == id);
-    if (employee is null){
-      return Results.NotFound("Employee is NotFound ");
-      
-    }
-    
-    employees.Remove(employee);
-    return Results.Ok($"Employee with ID {id} deleted");
-    }); 
-
 app.Run();
 
 class Employee
