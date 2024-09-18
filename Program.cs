06cd6211deb307f4aca5faf93c658aa6babb44c6
ghadah <gh.gadahd01@gmail.com>
2024-09-18 10:33:42 +0300

    Add Entities

--- Program.cs
+++ Program.cs
@@ -3,6 +3,19 @@ var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
 app.UseHttpsRedirection();
-Console.WriteLine($"Text");
+
+// create list of employees.
+List<Employee> employees = new List<Employee>();
 
 app.Run();
+
+class Employee
+{
+    public Guid Id { get; set; }
+    public string? FirstName { get; set; }
+    public string? LastName { get; set; }
+    public string? Email { get; set; }
+    public string? Position { get; set; }
+    public decimal Salary { get; set; }
+    public DateTime CreatedAt { get; set; }
+}
